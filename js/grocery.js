d44678aecad80ef2773eb505711c2c811b2a24c4
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-09-16 16:53:28 +0200

    ej 6 bug

--- js/grocery.js
+++ js/grocery.js
@@ -78,10 +78,12 @@ function addToCartList(id) {
 
     // 2. Add found product to the cartList array
         cartList.push(products.find(isId))
-        console.log(cartList);
+        // console.log(cartList);
         // console.log(cartList.type);
         calculateSubtotals();
         calculateTotal();
+        applyPromotionsSubtotals();
+        generateCart();
 }
 
 // Exercise 2
@@ -96,28 +98,31 @@ function calculateSubtotals() {
         subtotal.clothes.value = 0;
         subtotal.beauty.value = 0;
 
-        console.log(cartList.length)
      for(i = 0; i < cartList.length; i++){
         // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
         if(cartList[i].type === 'grocery'){
             subtotal.grocery.value += cartList[i].price;
             
-             console.log("subtotalGrocery", subtotal.grocery.value);
+            //  console.log("subtotalGrocery", subtotal.grocery.value);
 
         }else if(cartList[i].type === 'clothes'){
             subtotal.clothes.value += cartList[i].price;
 
-            console.log("subtotalClothes", subtotal.clothes.value);
+            // console.log("subtotalClothes", subtotal.clothes.value);
 
         }else if(cartList[i].type === 'beauty'){
             subtotal.beauty.value += cartList[i].price;
 
-            console.log("subtotalBeauty", subtotal.beauty.value);
+            // console.log("subtotalBeauty", subtotal.beauty.value);
 
         }else{
             return
         }
     }
+    // console.log("subtotalGrocery", subtotal.grocery.value);
+    // console.log("subtotalClothes", subtotal.clothes.value);
+    // console.log("subtotalBeauty", subtotal.beauty.value);
+
 }
 
 // Exercise 4
@@ -127,23 +132,114 @@ function calculateTotal() {
     for(let productType in subtotal){
         
             total += Object.values(subtotal[productType]).reduce((t, n) => t + n);
-            console.log("productValue: ", subtotal[productType]);
+            // console.log("productValue: ", subtotal[productType]);
 
     }
-    console.log("Total: ", total);
+    // console.log("Total: ", total);
 
 }
 
 // Exercise 5
 function applyPromotionsSubtotals() {
+    subtotal.grocery.discount= 0;
+    subtotal.clothes.discount = 0;
+    subtotal.beauty.discount = 0;
 
+    let oilProdNum = 0;
+    let mixtureProdNum = 0;
+
+    let oilDiscount = 0;
+    let mixtureDiscount = 0;
+    for(let i = 0; i < cartList.length; i++){
+        if(cartList[i].name === 'cooking oil'){
+            oilProdNum++;
+        }else if(cartList[i].name === 'Instant cupcake mixture'){
+            mixtureProdNum++;
+        }
+    }
+
+    if(oilProdNum >= 4){
+        oilDiscount = (products[0].price - 10) * oilProdNum;
+    }
+    if(mixtureProdNum >= 11){
+        mixtureDiscount = (products[2].price - products[2].price * 2/3) * mixtureProdNum;
+    }
+    // console.log("Oil discount: ", oilDiscount);
+    // console.log("Mixture discount: ", mixtureDiscount);
+
+    // Finalment hauràs d'aplicar la rebaixa als subtotales
+    subtotal.grocery.value = subtotal.grocery.value - oilDiscount - mixtureDiscount;
+
+    // i tornar a calcular el total (cridant a la funció de l'exercici 4).
+    calculateTotal();
 }
 
 // Exercise 6
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    
+
+    // const found = cart.some(item => cartList.includes(item));
+    let preCart = JSON.parse(JSON.stringify(cartList));
+     console.log("item", preCart);
+    // cart = [];
+    for (let i = 0; i < preCart.length; i++){
+        
+        if(!preCart[i].quantity){
+            cartList[i].quantity = 1;
+            preCart = JSON.parse(JSON.stringify(cartList));
+        }else if(preCart[i].quantity){
+            cartList[i].quantity += 1;
+            preCart = JSON.parse(JSON.stringify(cartList));
+            cartSet = new Set(preCart);
+            cart = [...cartSet]
+            preCart = []
+        }
+
+        // if (!found){
+        //     preCart[i].quantity = 1;
+        //     cart.push(preCart[i]);
+        //     console.log("notfound");
+        // } else if(found){
+        //     let cartQtty = preCart[i].quantity + 1;
+
+        //     console.log("cartQtty",cartQtty);
+
+        //     preCart[i].quantity = cartQtty;  
+        //     console.log("found");
+        // }
+                //  cartList = []
+                 
+
+    }
+    // cartSet = new Set(preCart);
+    // // console.log("cartSet", cartSet);
+    // cart = [...cartSet]
+
+    // cart = cartList
+    //  cartList = cart;
+    // for (let i = 0; i < cartList.length; i++){
+    // for (let prod in cartList){
+    //     for( let prod2 in cart){
+    //         console.log("prod", prod, prod2);
+        //     if(prod !== prod2){
+        // return
+        // }
+    //     if(cartList[i] !== cart){
+    //         cartList[i].quantity = 1;
+    //         cart.push(cartList[i]);
+    //     }else if(cartList[i].id === cart.id){
+    //         // cart.find(e => e === cartList[i]).push(e.quantity++)        = cart.filter((x) => {return x === cartList[i]})
+    //         cart.quantity++;
+
+    //     }
+    // }
+    console.log("Cart: ", cart);
+    console.log("CartList: ", cartList);
+
 }
+    
 
 // Exercise 7
 function applyPromotionsCart() {
