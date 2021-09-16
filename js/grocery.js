4e1696ba7ecc3b8c4128e4d00edb0076307384aa
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-09-16 22:53:35 +0200

    ej 6 debug 2

--- js/grocery.js
+++ js/grocery.js
@@ -178,24 +178,36 @@ function applyPromotionsSubtotals() {
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    
+    let preCart = cartList.map((obj) => obj);
+
+    const cartWithoutReps = {}
+
+    for(let i = 0; i < preCart.length; i++){
+        cartWithoutReps[preCart[i].quantity] = cartWithoutReps[preCart[i].quantity] == null ? preCart[i].quantity = 1 : preCart[i].quantity += 1;
+        // preCart[i].push(cartWithoutReps);
+    }
 
     // const found = cart.some(item => cartList.includes(item));
-    let preCart = JSON.parse(JSON.stringify(cartList));
-     console.log("item", preCart);
+    // let preCart = JSON.parse(JSON.stringify(cartList));
+    //  console.log("item", preCart);
     // cart = [];
-    for (let i = 0; i < preCart.length; i++){
+    // for (let i = 0; i < preCart.length; i++){
         
-        if(!preCart[i].quantity){
-            cartList[i].quantity = 1;
-            preCart = JSON.parse(JSON.stringify(cartList));
-        }else if(preCart[i].quantity){
-            cartList[i].quantity += 1;
-            preCart = JSON.parse(JSON.stringify(cartList));
-            cartSet = new Set(preCart);
-            cart = [...cartSet]
-            preCart = []
-        }
+    //     if(!preCart[i].quantity){
+    //         cartList[i].quantity = 1;
+    //         preCart = JSON.parse(JSON.stringify(cartList));
+    //     }else if(preCart[i].quantity){
+    //         cartList[i].quantity += 1;
+    //         preCart = JSON.parse(JSON.stringify(cartList));
+             cartSet = new Set(preCart);
+             cart = [...cartSet]
+    //         preCart = []
+    //     }
+        console.log("cartWithoutReps: ", preCart);
+
+        console.log("Cart: ", cart);
+        console.log("CartList: ", cartList);
+    
 
         // if (!found){
         //     preCart[i].quantity = 1;
@@ -212,7 +224,7 @@ function generateCart() {
                 //  cartList = []
                  
 
-    }
+    // }
     // cartSet = new Set(preCart);
     // // console.log("cartSet", cartSet);
     // cart = [...cartSet]
@@ -235,8 +247,6 @@ function generateCart() {
 
     //     }
     // }
-    console.log("Cart: ", cart);
-    console.log("CartList: ", cartList);
 
 }
     
