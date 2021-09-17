e337b27cadc8c2ef5653bafeb3f67a4290fe4874
Carlos <cbiosca82gmail.com>
2021-09-17 16:53:59 +0200

    Sprint Solution

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,52 @@
-// Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
-
-// Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+const form = document.querySelector('.form')
+const message = document.getElementById('message')
 
 // Exercise 9
-function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
+function validate (e) {
+    // Valida todos los campos. Si todos son correctos muestra un mensaje.
+    e.preventDefault()
+    for (let i = 0; i < form.length - 1; i++) {
+        checkInput(form[i])
+    }
+    for (let i = 0; i < form.length - 1; i++) {
+        console.log(form[i].nextElementSibling.classList);
+        if (form[i].nextElementSibling.classList.contains('show--error')) {
+            return
+        }
+    }
+    message.classList.add('valid')
+}
+
+function checkInput (input) {
+    const inputValue = input.value.trim()
+
+    if (inputValue === '' || inputValue === null) {
+        isError(input, 'Field is required')
+    } else if (inputValue.length > 0 && inputValue.length < 3) {
+        isError(input, 'Minimum 3 characters')
+    } else if (input.classList.contains('firstname') || input.classList.contains('lastname')) {
+        !/^[a-zA-Z]*$/g.test(inputValue) ? isError(input, 'Numbers in the name are not valid') : isValid(input)
+    } else if (input.classList.contains('email')) {
+        !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(inputValue) ? isError(input, 'Email address is not valid') : isValid(input)
+    } else if (input.classList.contains('password')) {
+        !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,8}$/.test(inputValue) ? isError(input, 'Your password must contain numbers and letters') : isValid(input)
+    } else if (input.classList.contains('phone')) {
+        /\D/.test(inputValue) ? isError(input, 'Letters in the phone are not valid') : isValid(input)
+    } else {
+        isValid(input)
+    }
+}
+
+// Si un campo no és válido muestra un mensaje en rojo
+function isError (input, message) {
+    input.classList.add('border--error')
+    input.nextElementSibling.innerText = message
+    input.nextElementSibling.classList.add('show--error')
+}
+
+// Si el campo és válido pone el borde en verde
+function isValid (input) {
+    input.classList.remove('border--error')
+    input.nextElementSibling.classList.remove('show--error')
+    input.classList.add('border--valid')
 }
--- js/grocery.js
+++ js/grocery.js
@@ -1,10 +1,20 @@
-// Exercise 11
-// Move this variable to a json file and load the data in this js
+// const grocery = document.getElementById('grocery')
+// const beauty = document.getElementById('beauty')
+// const clothes = document.getElementById('clothes')
+const clean = document.getElementById('clean')
+const items = document.getElementById('items')
+const table = document.getElementById('table')
+
+// He añadido más productos y las ofertas al array products 
 var products = [
     {
         name: 'cooking oil',
         price: 10.5,
-        type: 'grocery'
+        type: 'grocery',
+        offer: {
+            number: 3,
+            percent: 4.76
+        }
     },
     {
         name: 'Pasta',
@@ -14,15 +24,19 @@ var products = [
     {
         name: 'Instant cupcake mixture',
         price: 5,
-        type: 'grocery'
+        type: 'grocery',
+        offer: {
+            number: 10,
+            percent: 66
+        }
     },
     {
-        name: 'All-in-one',
+        name: 'All-in-1',
         price: 260,
         type: 'beauty'
     },
     {
-        name: 'Zero Make-up Kit',
+        name: 'Zero Makeup Kit',
         price: 20.5,
         type: 'beauty'
     },
@@ -45,79 +59,483 @@ var products = [
         name: 'Toddler Frock',
         price: 9.99,
         type: 'clothes'
+    },
+    {
+        name: 'Bestsellers',
+        price: 30,
+        type: 'books'
+    },
+    {
+        name: 'Kids',
+        price: 10,
+        type: 'books',
+        offer: {
+            number: 4,
+            percent: 20
+        }
+    },
+    {
+        name: 'Sci-fi',
+        price: 12,
+        type: 'books'
+    },
+    {
+        name: 'Mobile',
+        price: 150,
+        type: 'technology'
+    },
+    {
+        name: 'Tablet',
+        price: 300,
+        type: 'technology'
+    },
+    {
+        name: 'Desktop screen',
+        price: 120,
+        type: 'technology'
+    },
+    {
+        name: 'Bike',
+        price: 180,
+        type: 'sports'
+    },
+    {
+        name: 'Tennis kit',
+        price: 50,
+        type: 'sports',
+        offer: {
+            number: 2,
+            percent: 15
+        }
+    },
+    {
+        name: 'Soccer ball',
+        price: 99.99,
+        type: 'sports'
     }
 ]
+
 var cartList = [];
 var cart = [];
+
+// He añadido 3 categorias más a subtotal 
 var subtotal = {
     grocery: {
-        value: 0, 
+        value: 0,
         discount: 0
     },
     beauty: {
-        value: 0, 
+        value: 0,
         discount: 0
     },
     clothes: {
-        value: 0, 
+        value: 0,
         discount: 0
     },
+    books: {
+        value: 0,
+        discount: 0
+    },
+    technology: {
+        value: 0,
+        discount: 0
+    },
+    sports: {
+        value: 0,
+        discount: 0
+    }
 };
 var total = 0;
 
 // Exercise 1
-function addToCartList(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+function addToCartList (id) {
+    // Añade producto a cartList 
+    // Llama a calcular los subtotales 
+    for (let product of products) {
+        if (product.name.toLowerCase() == id.toLowerCase()) {
+            cartList.push(product)
+            calculateSubtotals()
+            console.log('cart item added', cart);
+            console.log('cartList product added', cartList);
+            console.log('subtotal', subtotal)
+            console.log('total', total);
+        }
+    }
+
 }
 
 // Exercise 2
-function cleanCart() {
-
+function cleanCart () {
+    // Vacia cartList y cart 
+    // Llama a calcular los subtotales y vacia el modal cart
+    cartList = []
+    cart = []
+    calculateSubtotals()
+    printCart()
+    console.log('cart empty', cart);
+    console.log('cartList empty', cartList);
+    console.log('subtotal', subtotal)
+    console.log('total', total);
 }
 
 // Exercise 3
-function calculateSubtotals() {
-    // 1. Create a for loop on the "cartList" array 
-    // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+function calculateSubtotals () {
+    // Calcula subtotal a partir de cartList
+    // Llama a calcular promociones 
+    for (let category in subtotal) {
+        subtotal[category].value = 0
+        subtotal[category].discount = 0
+    }
+    for (const product of cartList) {
+        subtotal[product.type].value += product.price
+    }
+    for (let category in subtotal) {
+        subtotal[category].value = subtotal[category].value.toFixed(2)
+    }
+
+    applyPromotionsSubtotals()
 }
 
 // Exercise 4
-function calculateTotal() {
-    // Calculate total price of the cart either using the "cartList" array
+function calculateTotal () {
+    // Calcula el total a partir de subtotal 
+    // Llama a generateCart. Esta comentado porque no uso esa función 
+    total = 0
+    for (const category in subtotal) {
+        total += subtotal[category].value - subtotal[category].discount
+    }
+    total = total.toFixed(2)
+
+    // generateCart()
 }
 
 // Exercise 5
-function applyPromotionsSubtotals() {
+function applyPromotionsSubtotals () {
+    // Comprueba si el producto tiene oferta, comprueba si la oferta se cumple y calcula el descuento que se aplica al subtotal 
+    // Llama a calcular el total 
+    for (const product of products) {
+        if (product.offer) {
+            let count = 0
+            for (const item of cartList) {
+                if (item.name === product.name) count++
+            }
+            if (count >= product.offer.number) {
+                subtotal[product.type].discount += count * (product.price * product.offer.percent / 100)
+                subtotal[product.type].discount = subtotal[product.type].discount.toFixed(2)
+            }
+        }
+    }
 
+    calculateTotal()
 }
 
 // Exercise 6
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+function generateCart () {
+    // Genera cart a partir de carList comprobando que no haya productos repetidos 
+    // Esta funcion la sustituye addToCart por eso esta comentada y no la uso 
+
+    // for (let item of cart) {
+    //     if (item.name == cartList.at(-1).name) {
+    //         item.quantity++
+    //         item.subtotal = item.quantity * cartList.at(-1).price
+    //         applyPromotionsCart(item)
+    //         console.log('cart item added', cart);
+    //         return
+    //     }
+    // }
+    // let newItem = {
+    //     name: cartList.at(-1).name,
+    //     price: cartList.at(-1).price,
+    //     type: cartList.at(-1).type,
+    //     quantity: 1,
+    //     subtotal: 1 * cartList.at(-1).price,
+    //     subtotalWithDiscount: 1 * cartList.at(-1).price
+    // }
+    // cart.push(newItem)
+    // console.log('cart item added', cart);
 }
 
+
 // Exercise 7
-function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+function applyPromotionsCart (item) {
+    // Comprueba si el producto tiene oferta y si ha comprado la cantidad necesaria 
+    // Añade el descuento al item de cart 
+    for (const product of products) {
+        if (item.name === product.name && product.offer && item.quantity >= product.offer.number) {
+            item.subtotalWithDiscount = item.quantity * (product.price * product.offer.percent / 100)
+            item.subtotalWithDiscount = (item.subtotalWithDiscount).toFixed(2)
+        }
+    }
 }
 
 // Exercise 8
-function addToCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+function addToCart (id) {
+    // Añade el producto a cart, y luego también a cartList. 
+    let productIndex = id - 1
+    for (let item of cart) {
+        if (item.name == products[productIndex].name) {
+            item.quantity++
+            item.subtotal = item.quantity * products[productIndex].price
+            item.subtotal = item.subtotal.toFixed(2)
+            addToCartList(products[productIndex].name)
+            applyPromotionsCart(item)
+            return
+        }
+    }
+    let newItem = {
+        name: products[productIndex].name,
+        price: products[productIndex].price,
+        type: products[productIndex].type,
+        quantity: 1,
+        subtotal: 1 * products[productIndex].price,
+        subtotalWithDiscount: 1 * products[productIndex].price
+    }
+    cart.push(newItem)
+
+    addToCartList(products[productIndex].name)
 }
-
-// Exercise 9
-function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
-
-
 
 // Exercise 10
-function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+function removeFromCart (id) {
+    // Disminuye en 1 la cantidad del item de cart, y si es el último lo elimina 
+    // Despúes lo remueve de cartList 
+    for (let item of cart) {
+        if (item.name === products[id].name) {
+            if (item.quantity > 1) {
+                item.quantity--
+                item.subtotal = item.quantity * products[id].price
+                item.subtotal = item.subtotal.toFixed(2)
+            } else {
+                const index = cart.indexOf(item)
+                cart.splice(index, 1)
+            }
+            removeFromCartList(item.name)
+            applyPromotionsCart(item)
+        }
+    }
 }
+
+function removeFromCartList (name) {
+    for (let product of cartList) {
+        if (product.name === name) {
+            const index = cartList.indexOf(product)
+            cartList.splice(index, 1)
+            calculateSubtotals()
+            console.log('cart item removed', cart);
+            console.log('cartList product removed', cartList);
+            console.log('subtotal', subtotal)
+            console.log('total', total);
+            return
+        }
+    }
+}
+
+
+
+// Exercise 11
+function printCart () {
+    // Rellena el cart modal manipulando el DOM 
+    items.innerHTML = ' '
+    table.innerHTML = ''
+
+    if (cart.length === 0) {
+        const empty = document.createElement('td')
+        empty.classList.add('text-center', 'mx-auto', 'p-2', 'display-4')
+        empty.colSpan = 5
+        empty.textContent = 'Empty'
+        items.appendChild(empty)
+    } else {
+        printHeadList()
+        printBodyList()
+        printCategories()
+    }
+}
+
+// Titulos tabla lista de productos 
+function printHeadList () {
+    const titles = ['Product', 'Price', 'Quantity', 'Total']
+    const fragment = document.createDocumentFragment()
+
+    const head = document.createElement('thead')
+    head.classList.add('thead-light')
+    const tr = document.createElement('tr')
+    tr.classList.add('text-center')
+    for (let title of titles) {
+        const th = document.createElement('th')
+        th.scope = 'col'
+        th.textContent = title
+        tr.appendChild(th)
+    }
+    const th = document.createElement('th')
+    tr.appendChild(th)
+
+    head.appendChild(tr)
+    fragment.appendChild(head)
+    items.appendChild(fragment)
+}
+
+// Tabla lista de productos 
+function printBodyList () {
+    const fragment = document.createDocumentFragment()
+    const body = document.createElement('tbody')
+
+    for (const item of cart) {
+        const fila = document.createElement('tr')
+        fila.classList.add('text-center', 'item__row')
+        const name = document.createElement('td')
+        name.classList.add('text-left')
+        name.textContent = item.name
+        const price = document.createElement('td')
+        price.textContent = `$${item.price}`
+        const quantity = document.createElement('td')
+        quantity.textContent = item.quantity
+        const total = document.createElement('td')
+        total.textContent = `$${item.subtotal}`
+        const remove = document.createElement('td')
+        const button = document.createElement('button')
+        button.classList.add('btn', 'btn-secondary', 'item__button')
+        button.textContent = '-'
+
+        remove.appendChild(button)
+
+        fila.appendChild(name)
+        fila.appendChild(price)
+        fila.appendChild(quantity)
+        fila.appendChild(total)
+        fila.appendChild(remove)
+        body.appendChild(fila)
+        fragment.appendChild(body)
+    }
+    items.appendChild(fragment)
+}
+
+// Tabla de categorias 
+function printCategories () {
+    printHeadCategories()
+    printBodyCategories()
+}
+
+function printHeadCategories () {
+    const titles = ['Subtotal', 'Discount', 'Total']
+    const fragment = document.createDocumentFragment()
+
+    const head = document.createElement('thead')
+    head.classList.add('thead-light')
+    const trhead = document.createElement('tr')
+    trhead.classList.add('text-center')
+    const thhead = document.createElement('th')
+    thhead.scope = 'col'
+    trhead.appendChild(thhead)
+    for (let title of titles) {
+        const thhead = document.createElement('th')
+        thhead.scope = 'col'
+        thhead.textContent = title
+        trhead.appendChild(thhead)
+    }
+
+    head.appendChild(trhead)
+    fragment.appendChild(head)
+    table.appendChild(fragment)
+}
+
+function printBodyCategories () {
+    let types = new Set()
+    for (let item of cart) {
+        types.add(item.type)
+    }
+    const fragment = document.createDocumentFragment()
+
+
+    const body = document.createElement('tbody')
+    for (let type of types) {
+        const tr = document.createElement('tr')
+        tr.classList.add('text-center')
+        const th = document.createElement('th')
+        th.scope = 'row'
+        th.classList.add('text-capitalize')
+        th.textContent = type
+        const value = document.createElement('td')
+        value.textContent = `$${subtotal[type].value}`
+        const discount = document.createElement('td')
+        discount.textContent = `-$${subtotal[type].discount}`
+        const valuediscount = document.createElement('td')
+        const rest = parseFloat(subtotal[type].value - subtotal[type].discount).toFixed(2)
+        valuediscount.textContent = `$${rest}`
+
+        tr.appendChild(th)
+        tr.appendChild(value)
+        tr.appendChild(discount)
+        tr.appendChild(valuediscount)
+        body.appendChild(tr)
+    }
+
+    const trtotal = document.createElement('tr')
+    trtotal.classList.add('text-center')
+    const thtotal = document.createElement('th')
+    thtotal.scope = 'row'
+    thtotal.colSpan = 3
+    thtotal.classList.add('text-right')
+    thtotal.textContent = 'TOTAL PRICE'
+    const tdtotal = document.createElement('td')
+    tdtotal.textContent = `$${total}`
+
+    trtotal.appendChild(thtotal)
+    trtotal.appendChild(tdtotal)
+    body.appendChild(trtotal)
+
+    fragment.appendChild(body)
+    table.appendChild(fragment)
+}
+
+//Events
+
+//Eventos para addToCartList que no uso porque los sustituye addToCart
+// grocery.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('btn-primary')) {
+//         addToCartList(e.target.previousElementSibling.previousElementSibling.innerText)
+//     }
+// });
+
+// beauty.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('btn-primary')) {
+//         addToCartList(e.target.previousElementSibling.previousElementSibling.innerText)
+//     }
+// });
+
+// clothes.addEventListener('click', (e) => {
+//     if (e.target.classList.contains('btn-primary')) {
+//         addToCartList(e.target.previousElementSibling.previousElementSibling.innerText)
+//     }
+// });
+
+// Comprueba que productos tienen ofertas y las pinta al cargar la página
+window.addEventListener('DOMContentLoaded', () => {
+    for (let product of products) {
+        if (product.offer) {
+            const category = document.getElementById(`${product.type}`).children[1]
+            for (let card of category.children) {
+                if (card.children[0].children[1].children[0].textContent === product.name) card.children[0].children[0].children[1].textContent = `Buy ${product.offer.number} / ${product.offer.percent}% OFF`
+            }
+
+        }
+    }
+
+});
+
+// Elimina todos lo productos del carrito al pulsar clean cart
+clean.addEventListener('click', () => {
+    cleanCart()
+})
+
+// Actualiza cart modal cuando se elemina un producto de la lista pulsando boton - 
+items.addEventListener('click', (e) => {
+    if (e.target.classList.contains('item__button')) {
+        for (const product of products) {
+            if (product.name === e.target.parentElement.parentElement.firstElementChild.textContent) {
+                const index = products.indexOf(product)
+                removeFromCart(index)
+                printCart()
+            }
+        }
+    }
+})
